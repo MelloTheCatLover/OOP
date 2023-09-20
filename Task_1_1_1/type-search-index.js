d11104f258111aa432e704332a1068aa78d3e190
MelloTheCatLover <MelloTheCatLover@users.noreply.github.com>
2023-09-20 09:34:01 +0000

    Deploying to gh-pages from @ MelloTheCatLover/OOP@5ea1c3d5f22dddc99b640494cafeb110c0026113 🚀

--- Task_1_1_1/type-search-index.js
+++ Task_1_1_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.bykova","l":"Main"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.kozoliy","l":"Main"}]
