dcea6cdf64593a012644f0070d3fbd51442e8d93
MelloTheCatLover <misha.kozoliy@gmail.com>
2023-11-06 18:19:16 +0700

    мелкие иправления

--- Task_1_2_2/src/main/java/ru/nsu/kozoliy/GraphLaws.java
+++ Task_1_2_2/src/main/java/ru/nsu/kozoliy/GraphLaws.java
@@ -126,6 +126,7 @@ public abstract class GraphLaws<T> {
 
     /**
      * Класс вершины и расстояния
+     *
      */
     protected static class VertexDistance<T> implements Comparable<VertexDistance> {
         protected Vertex<T> vertex;
