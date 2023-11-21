8c4a540f0a1bf9db91eb1f162cdeda269c6a503d
MelloTheCatLover <misha.kozoliy@gmail.com>
2023-11-22 00:18:46 +0700

    Rewiew dog 1

--- Task_1_4_1/src/main/java/ru/nsu/kozoliy/Pair.java
+++ Task_1_4_1/src/main/java/ru/nsu/kozoliy/Pair.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 /**
  * <p>A convenience class to represent name-value pairs.</p>
+ *
  * @since JavaFX 2.0
  */
 
@@ -12,10 +13,11 @@ public class Pair<K, V> implements Serializable {
     /**
      * Key of this <code>Pair</code>.
      */
-    private K key;
+    private final K key;
 
     /**
      * Gets the key for this pair.
+     *
      * @return key for this pair
      */
     public K getKey() {
@@ -23,12 +25,13 @@ public class Pair<K, V> implements Serializable {
     }
 
     /**
-     * Value of this this <code>Pair</code>.
+     * Value of this <code>Pair</code>.
      */
     private V value;
 
     /**
      * Gets the value for this pair.
+     *
      * @return value for this pair
      */
     public V getValue() {
@@ -36,8 +39,10 @@ public class Pair<K, V> implements Serializable {
     }
 
     /**
-     * Creates a new pair
+     * Creates a new pair.
+     *
      * @param key The key for this pair
+     *
      * @param value The value to use for this pair
      */
     public Pair(K key, V value) {
@@ -59,7 +64,7 @@ public class Pair<K, V> implements Serializable {
     }
 
     /**
-     * <p>Generate a hash code for this <code>Pair</code>.</p>
+     * <p>Generate a hash code for this <code>Pair</code>.</p>.
      *
      * <p>The hash code is calculated using both the name and
      * the value of the <code>Pair</code>.</p>
@@ -75,32 +80,23 @@ public class Pair<K, V> implements Serializable {
     }
 
     /**
-     * <p>Test this <code>Pair</code> for equality with another
+     * <p>Test this <code>Pair</code> for equality with another.
      * <code>Object</code>.</p>
-     *
-     * <p>If the <code>Object</code> to be tested is not a
-     * <code>Pair</code> or is <code>null</code>, then this method
-     * returns <code>false</code>.</p>
      *
      * <p>Two <code>Pair</code>s are considered equal if and only if
      * both the names and values are equal.</p>
-     *
-     * @param o the <code>Object</code> to test for
-     * equality with this <code>Pair</code>
-     * @return <code>true</code> if the given <code>Object</code> is
-     * equal to this <code>Pair</code> else <code>false</code>
      */
     @Override
     public boolean equals(Object o) {
-        if (this == o)
+        if (this == o) {
             return true;
+        }
         if (o instanceof Pair) {
             Pair pair = (Pair) o;
-            if (key != null ? !key.equals(pair.key) : pair.key != null)
+            if (key != null ? !key.equals(pair.key) : pair.key != null) {
                 return false;
-            if (value != null ? !value.equals(pair.value) : pair.value != null)
-                return false;
-            return true;
+            }
+            return value != null ? value.equals(pair.value) : pair.value == null;
         }
         return false;
     }
