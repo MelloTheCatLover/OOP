322c7bda871649ad557bc08faf3dec430d1ac159
MelloTheCatLover <misha.kozoliy@gmail.com>
2024-02-19 18:18:05 +0700

    review dog 7

--- Task_2_1_1/src/main/java/ru/nsu/kozoliy/ChartBuilder.java
+++ Task_2_1_1/src/main/java/ru/nsu/kozoliy/ChartBuilder.java
@@ -100,6 +100,7 @@ public class ChartBuilder {
             arrayOfResultLargeArray[numOfThreads - 2] = timeElapsedParallel;
         }
 
+        /*
         // Create and display the chart
         javax.swing.SwingUtilities.invokeLater(() -> {
             JFrame frame = new JFrame("Execution Time Comparison");
@@ -166,6 +167,6 @@ public class ChartBuilder {
             frame.setLocationRelativeTo(null);
             frame.setVisible(true);
         });
-
+        */
     }
 }
