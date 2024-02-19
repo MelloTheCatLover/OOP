8e646192b49cc42293b948b7eb88d1c5781b274c
MelloTheCatLover <MelloTheCatLover@users.noreply.github.com>
2024-02-19 09:57:57 +0000

    Deploying to gh-pages from @ MelloTheCatLover/OOP@2edabde4f936f9d5a74252cd6020d8c4458c0e28 🚀

---
+++ Task_2_1_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.kozoliy","l":"ChartBuilder"},{"p":"ru.nsu.kozoliy","l":"ExcludeFromJacocoGeneratedReport"},{"p":"ru.nsu.kozoliy","l":"ParallelDetector"},{"p":"ru.nsu.kozoliy","l":"ParallelStreamDetector"},{"p":"ru.nsu.kozoliy","l":"SequentialDetector"}]
