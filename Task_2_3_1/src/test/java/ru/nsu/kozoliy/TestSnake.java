05b749e406d4ba5ac8f0661d128919da5e9c9c57
MelloTheCatLover <misha.kozoliy@gmail.com>
2024-05-08 00:42:13 +0700

    fixin review problems 8

--- Task_2_3_1/src/test/java/ru/nsu/kozoliy/TestSnake.java
+++ Task_2_3_1/src/test/java/ru/nsu/kozoliy/TestSnake.java
@@ -1,8 +1,8 @@
 package ru.nsu.kozoliy;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class TestSnake {
