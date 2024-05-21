a4f5314636d4953cbc4004c06108c4878b43e362
MelloTheCatLover <MelloTheCatLover@users.noreply.github.com>
2024-05-21 20:35:24 +0000

    Deploying to gh-pages from @ MelloTheCatLover/OOP@eb04980e19f81e983f39b76368c1147f664c169b 🚀

---
+++ Task_2_4_1/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"ru.nsu.kozoliy"},{"l":"ru.nsu.kozoliy.dslFilesPackage"},{"l":"ru.nsu.kozoliy.model.configs"},{"l":"ru.nsu.kozoliy.model.object"},{"l":"ru.nsu.kozoliy.model.tasks"}]
