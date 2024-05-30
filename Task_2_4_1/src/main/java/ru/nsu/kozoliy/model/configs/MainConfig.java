808fe06f120d0677a06f3b1a819e0e0451c4a5ee
MelloTheCatLover <misha.kozoliy@gmail.com>
2024-05-31 02:07:48 +0700

    achievements second version

--- Task_2_4_1/src/main/java/ru/nsu/kozoliy/model/configs/MainConfig.java
+++ Task_2_4_1/src/main/java/ru/nsu/kozoliy/model/configs/MainConfig.java
@@ -1,13 +1,26 @@
 package ru.nsu.kozoliy.model.configs;
 
+import groovy.lang.Closure;
 import lombok.Data;
+import ru.nsu.kozoliy.dslFilesPackage.DslDelegate;
 import ru.nsu.kozoliy.model.object.Achievement;
 
 import java.util.List;
 
 @Data
 public class MainConfig {
+
     private List<Achievement> enabledAchievements;
+    Git git = new Git();
+    Evaluation evaluation = new Evaluation();
+
+    public void git(Closure<?> closure) {
+        DslDelegate.groovyDelegate(git, closure);
+    }
+
+    public void evaluation(Closure<?> closure) {
+        DslDelegate.groovyDelegate(evaluation, closure);
+    }
 
     public List<Achievement> getEnabledAchievements() {
         return enabledAchievements;
@@ -23,4 +36,8 @@ public class MainConfig {
                 "enabledAchievements=" + enabledAchievements +
                 '}';
     }
+
+
+
 }
+
